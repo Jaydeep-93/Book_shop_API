9ccde9dd42c391d554ec8b4e8fe66a30781e9a5f
Jay <jaydeepkachare123@gmail.com>
2022-09-10 10:24:04 +0530

    morgan added

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-const express = require('express'); 
+const express = require("express");
+const morgan = require("morgan");
 
-const bookRouter = require('./router/book'); 
+const bookRouter = require("./router/book");
 
-const app = express(); 
+// creating server app
+const app = express();
 
-app.use('/book', bookRouter); 
+// middlewar esetup for app wise use
+app.use(morgan("dev"));
 
+// routes added
+app.use("/book", bookRouter);
+
+// if not route found
 app.use((req, res, next) => {
   res.status(400).json({ message: "Page Not found" });
 });
 
+// error handler middleware for all error thrown
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message,
+        stack: err.stack,
+    });
+});
+
+// starting server
 app.listen(3000, () => {
-    console.log('server started on PORT: 3000');
-})
+    console.log("server started on PORT: 3000");
+});
